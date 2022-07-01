f3c8af24fe0b1fa146a3d82458d7856ff7d525fa
Amir.x.One <Amir.x.one.2000@gmail.com>
2022-07-01 12:57:54 +0430

    Create Navbar

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,7 @@ const Main = () => {
         <div className='h-[100vh] w-full relative'>
             <video className=' w-full h-full' src={mainBg} autoPlay loop muted />
             <div className='w-full h-full absolute top-0 left-0 flex justify-center items-center'>
-                <h1>Desentr</h1>
+                <h1>Desentralized</h1>
                 <h1></h1>
             </div>
         </div>
